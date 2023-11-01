22897045f3a09076928959300c8b3b136dedb5c4
minbert <sukmin28@hermesys.co.kr>
2023-11-01 09:52:36 +0900

    chore: compile ui 코드 변경

--- Drainage/ui/compile_ui.bat
+++ Drainage/ui/compile_ui.bat
@@ -2,10 +2,6 @@
 call "C:\Program Files\QGIS 3.34.0\bin\o4w_env.bat"
 
 @echo on
-pyuic5 drainage_dialog_base.ui > drainage_dialog_base.py
-pyuic5 Catchment_dialog_base.ui > Catchment_dialog_base.py
-pyuic5 Drainage_dockwidget_base.ui > Drainage_dockwidget_base.py
-pyuic5 FillSink_dialog_base.ui > FillSink_dialog_base.py
-pyuic5 Flat_dialog_base.ui > Flat_dialog_base.py
-pyuic5 Flow_Accumulation_dialog_base.ui > Flow_Accumulation_dialog_base.py
-pyuic5 Flow_Direction_dialog_base.ui > Flow_Direction_dialog_base.py
+pyuic5 Slope_dialog_base.ui > Slope_dialog_base.py
+pyuic5 Stream_Definition_dialog_base.ui > Stream_Definition_dialog_base.py
+pyuic5 Watershed_dialog_base.ui > Watershed_dialog_base.py
